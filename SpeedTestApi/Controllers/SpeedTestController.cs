52979ee61634933bc36f005491250eb7af198325
oivind <oeku@computas.com>
2020-02-12 13:20:35 +0100

    Added /speedtest/ping

---
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpeedTestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpeedTestController : ControllerBase
+    {
+        // GET speedtest/ping
+        [Route("ping")]
+        [HttpGet]
+        public string Ping()
+        {
+            return "PONG";
+        }
+
+    }
+}
